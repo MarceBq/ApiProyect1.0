c39d0daa9739018f5f14072f735e3a7bf9984fd1
MarceloBq <marcebq2005@gmail.com>
2023-05-16 00:53:17 -0500

    final commit

--- script.js
+++ script.js
@@ -1,58 +1,72 @@
- 
 
+//Varible global
+let variableGlobal;
+
+
+//Capturo el nodo del search
+const searchInput = document.querySelector('.search') 
+const selectUl = document.querySelector('.suggestions')
+ 
 //capturar el api
 const selectApi = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 fetch(selectApi)
     .then(response => response.json())
-    .then(json => showData(json))
-
-
-const showData = data => {
-    let createList = ` `
-
-    for(let i = 0; i < data.length; i++){
-
-        createList += `
-            <li>
-             
-                <span class="name">${data[i].city}, ${data[i].state}</span>
-                <span class="population">${data[i].population}</span>
-             
-            </li>`
-    }
-
-    document.querySelector('ul').innerHTML = createList
-
-    // const lists = document.querySelectorAll('li')
-    // console.log(lists)
-}
-
+    .then(data =>  variableGlobal = data)
  
 
+searchInput.addEventListener("keyup", e => {
+     
+  //Arreglo para guardar los valores
+  let newArray = [] 
+ 
+  //valor del input
+  const inputValue = e.target.value
 
+  const arraysFilter = variableGlobal.filter(({city, state}) => city.includes(inputValue) 
+  || state.includes(inputValue))
+  
+  
+  // const searcherStyle = variableGlobal.filter(({city, state}) => city.includes(inputValue)  
+  // || state.includes(inputValue))
+  
  
-// console.log(list)
- 
-document.addEventListener("keyup", e => {
-    if (e.target.matches('.search')) {
-      
-      const text = e.target.value.toLowerCase();
-      
-      document.querySelectorAll('li .name.population').forEach(el => {
-        if (el.textContent.toLowerCase().includes(text)) {
-          el.classList.remove('ocultar');
-        } else {
-          el.classList.add('ocultar');
-        }
-      });
-    }
+  arraysFilter.forEach(({city, state, population}) => {
+    
+    const highlight = `<span class="hl">${inputValue}</span>`
+
+    newArray += `<li>
+    <span>${city.replace(new RegExp(inputValue, 'gi'), highlight)}, ${state.replace(new RegExp(inputValue, 'gi'), highlight)}</span>
+    <span class="population">${population}</span>
+    </li>
+    `
  
     
+    }); 
+
+    selectUl.innerHTML = newArray
+  
+
+    
+
+
+    
+
+    // const wordCities = ' ';
+
+    // const highlightAngeles = `Los <span class="hl">Angeles</span>`
+
+
+
+
+    
+  // searcherStyle.forEach(({city, state}) => {
+
+  //   `Los <span class="hl">${city}</span>`
+    
+  // })
+
+
+   
 });
 
-
-
-
-
  
-
